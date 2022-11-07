0445466a3effde650d615481eed4ad6570162c65
biruk ayalew <se.biruk.ayalew@gmail.com>
2022-11-07 16:07:23 +0300

    add core and utils

---
+++ lib/constants/strings/app_routes.dart
@@ -0,0 +1,16 @@
+class GojoRoutes {
+  static const root = '/root';
+  static const home = '/home';
+  static const contacts = '/contacts';
+  static const chat = '/chat';
+  static const signin = '/signin';
+  static const register = '/register';
+  static const otp = '/otp';
+  static const map = '/map';
+  static const myAppointments = '/myAppointments';
+  static const propertyDetail = 'property detail';
+  static const appointment = 'appointment';
+  static const virtualTour = 'virtual tour';
+  static const applications = 'applications';
+  static const app = '/';
+}
