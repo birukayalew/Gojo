45919eece90fd659215d78df3d710e186a57097a
biruk ayalew <se.biruk.ayalew@gmail.com>
2022-11-08 23:22:15 +0300

    add virtual tour

---
+++ lib/features/virtual_tour/presentation/screen/virutal_tour.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../../../Gojo-Mobile-Shared/UI/widgets/parent_view.dart';
+
+class VirtualTourView extends StatefulWidget {
+  const VirtualTourView({super.key});
+
+  @override
+  State<VirtualTourView> createState() => _VirtualTourViewState();
+}
+
+class _VirtualTourViewState extends State<VirtualTourView> {
+  late final WebViewController controller;
+  final virtualTourUrl = 'https://vr-demo-git-main-par0s.vercel.app/';
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(
+        Uri.parse(virtualTourUrl),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GojoParentView(
+      isFullScreen: true,
+      child: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
